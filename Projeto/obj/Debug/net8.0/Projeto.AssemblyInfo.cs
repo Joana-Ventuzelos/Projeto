41e290241241107f77ee38580cbe8630c893c647
Joana-Ventuzelos <jr_ventuzelos@hotmail.com>
2025-03-13 16:07:49 +0000

    final com alteraçoes com\n

--- Projeto/obj/Debug/net8.0/Projeto.AssemblyInfo.cs
+++ Projeto/obj/Debug/net8.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b91654c7e74bf63b538449f34fa3686d4bc959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11349626dcac026dede9738812d84600778904b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
